6274222827b48c211870b98558d51de21f268e6b
MugiSus <mmgiss256@gmail.com>
2025-02-27 00:44:40 +0900

    Center ParticleBackground div regardless of window size [commit by roo code]

--- src/components/ParticleBackground/index.tsx
+++ src/components/ParticleBackground/index.tsx
@@ -4,7 +4,7 @@ import Scene from './Scene';
 
 export default function ParticleBackground() {
   return (
-    <div className="fixed inset-0 -z-10 m-auto h-[640px] w-[1152px]">
+    <div className="fixed left-1/2 top-1/2 -z-10 h-[640px] w-[1152px] -translate-x-1/2 -translate-y-1/2">
       <Scene />
     </div>
   );

85b29c68cfa0e60561b82bedc543b485769294c2
MugiSus <mmgiss256@gmail.com>
2025-02-27 20:41:47 +0900

    feat: Update font configuration in layout.tsx to use 'Inter' font with improved class assignment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-  weight: 'variable',
-});
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter', weight: 'variable' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={inter.variable}
-      >
+    <html lang="en" className={inter.variable}>
+      <body className="font-inter">
         {children}
       </body>
     </html>

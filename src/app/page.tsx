3d632be3e8d592687b7eb7d1805b57a6e492e1b3
MugiSus <mmgiss256@gmail.com>
2025-02-27 20:41:53 +0900

    feat: Add XCheckbox component and update page layout with improved spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import ParticleBackground from '@/components/ParticleBackground';
 import { useState } from 'react';
 import { WORKS } from '@/constants/works';
+import XCheckbox from '@/components/XCheckbox';
 
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -12,10 +13,10 @@ export default function Home() {
       <ParticleBackground imagePath={WORKS[currentIndex].images[0]} />
 
       <main className="flex min-h-svh w-full flex-col items-center justify-between leading-loose tracking-tighter">
-        <div className="flex w-full items-center justify-center gap-2 bg-white p-8 text-xl font-light">
+        <div className="flex w-full items-center justify-center gap-2 bg-white px-8 py-12 text-xl font-light">
           Akira Wakita Lab.
         </div>
-        <div className="flex w-full justify-center gap-8 bg-white p-8 font-light">
+        <div className="flex w-full justify-center gap-8 bg-white px-8 py-12 font-light">
           <div className="scrollbar-hidden flex max-w-full gap-8 overflow-x-scroll">
             {WORKS.map((work, index) => (
               <button
@@ -23,14 +24,7 @@ export default function Home() {
                 onClick={() => setCurrentIndex(index)}
                 className="flex items-center gap-2 text-nowrap p-0 tracking-tighter"
               >
-                <div>
-                  <svg width="16" height="16" stroke="currentColor" fill="none" strokeWidth={1} viewBox="0 0 16 16">
-                    <path d="M2 2 L14 2 L14 14L2 14Z" />
-                    {index === currentIndex && (
-                      <path d="M2 2 L14 14 M14 2L2 14Z" />
-                    )}
-                  </svg>
-                </div>
+                <XCheckbox selected={index === currentIndex} />
                 {work.name}
               </button>
             ))}
---
+++ src/components/XCheckbox.tsx
@@ -0,0 +1,12 @@
+export default function XCheckbox({selected}: { selected: boolean }) {
+  return (
+    <div>
+      <svg width="16" height="16" stroke="currentColor" fill="none" strokeWidth={1} viewBox="0 0 16 16">
+        <path d="M2 2 L14 2 L14 14L2 14Z" />
+        {selected && (
+          <path d="M2 2 L14 14 M14 2L2 14Z" />
+        )}
+      </svg>
+    </div>
+  );
+}

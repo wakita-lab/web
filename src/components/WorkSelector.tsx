2dcdceea2b137bf7abd3daa45cedae81c471855b
MugiSus <mmgiss256@gmail.com>
2025-02-28 02:52:49 +0900

    feat: Update WorkSelector to accept works prop for dynamic rendering

--- src/components/WorkSelector.tsx
+++ src/components/WorkSelector.tsx
@@ -1,28 +1,35 @@
 import XCheckbox from '@/components/XCheckbox';
-import { WORKS } from '@/constants/works';
+import { Work } from '@/constants/works';
 
 interface WorkSelectorProps {
   currentIndex: number;
   onIndexChange: (index: number) => void;
+  works: Work[];
 }
 
-export default function WorkSelector({ currentIndex, onIndexChange }: WorkSelectorProps) {
+export default function WorkSelector({ currentIndex, onIndexChange, works }: WorkSelectorProps) {
+  const length = works.length;
+
   return (
     <div className="flex w-full items-center justify-center gap-4 bg-white px-6 py-12 md:gap-8 md:px-12">
-      <XCheckbox selected />
-      <div className="scrollbar-hidden flex max-w-full gap-0 overflow-x-scroll">
-        {WORKS.map((work, index) => (
+      <button onClick={() => currentIndex > 0 && onIndexChange(currentIndex - 1)}>
+        <XCheckbox selected={currentIndex === 0} />
+      </button>
+      <div className="scrollbar-hidden flex max-w-full gap-0 overflow-x-scroll md:gap-4">
+        {works.map((work, index) => (
           <button
             key={index}
             onClick={() => onIndexChange(index)}
             className="flex items-center gap-2 text-nowrap px-2 tracking-tighter"
           >
             <XCheckbox selected={index === currentIndex} />
-            {work.name.en}
+            item
           </button>
         ))}
       </div>
-      <XCheckbox selected />
+      <button onClick={() => currentIndex < length - 1 && onIndexChange(currentIndex + 1)}>
+        <XCheckbox selected={currentIndex === length - 1} />
+      </button>
     </div>
   );
 }

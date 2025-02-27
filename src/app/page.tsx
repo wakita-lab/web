c15abd6d4867fc25f5d9b56a6dccc997b485ca87
MugiSus <mmgiss256@gmail.com>
2025-02-28 01:39:45 +0900

    feat: Refactor layout and styling in Home component for improved responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,23 +12,25 @@ export default function Home() {
     <>
       <ParticleBackground imagePath={WORKS[currentIndex].images[0]} />
 
-      <main className="flex min-h-svh w-full flex-col items-center justify-between leading-loose tracking-tighter">
-        <div className="flex w-full items-center justify-center gap-2 bg-white px-8 py-12 text-xl font-light">
+      <main className="flex min-h-svh w-full flex-col items-center justify-between font-light leading-loose tracking-tighter">
+        <div className="flex w-full items-center justify-center bg-white px-6 py-12 text-lg md:gap-4 md:px-12">
           Akira Wakita Lab.
         </div>
-        <div className="flex w-full justify-center gap-8 bg-white px-8 py-12 font-light">
-          <div className="scrollbar-hidden flex max-w-full gap-8 overflow-x-scroll">
+        <div className="flex w-full items-center justify-center gap-2 bg-white px-6 py-12 md:gap-4 md:px-12">
+          <XCheckbox selected />
+          <div className="scrollbar-hidden flex max-w-full gap-4 overflow-x-scroll md:gap-8">
             {WORKS.map((work, index) => (
               <button
                 key={index}
                 onClick={() => setCurrentIndex(index)}
-                className="flex items-center gap-2 text-nowrap p-0 tracking-tighter"
+                className="flex items-center gap-2 text-nowrap px-2 tracking-tighter"
               >
                 <XCheckbox selected={index === currentIndex} />
                 {work.name.en}
               </button>
             ))}
           </div>
+          <XCheckbox selected />
         </div>
       </main>
     </>

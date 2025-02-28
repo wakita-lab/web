3a421ebc17bbf904c8a22dab7a5fc4eb268287ac
MugiSus <mmgiss256@gmail.com>
2025-02-28 15:01:57 +0900

    refactor: Simplify scroll handling by removing unused state and updating scroll amount calculation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,29 @@
 
 import ParticleBackground from '@/components/ParticleBackground';
 import { useCallback, useRef, useState } from 'react';
+
 import { WORKS } from '@/constants/works';
 import WorkSelector from '@/components/WorkSelector';
 
 const workHeight = 1000;
 
 export default function Home() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [scrollAmount, setScrollAmount] = useState(0);
   const [particleSpeed] = useState(0.015);
 
   const scrollFieldRef = useRef<HTMLDivElement>(null);
   const scrollFieldHeight = scrollFieldRef.current?.clientHeight || 0;
 
-  const handleScrollFieldScroll = useCallback(() => {
-    const scrollAmount = scrollFieldRef.current?.scrollTop || 0;
-    const index = Math.min(Math.trunc(scrollAmount / workHeight), WORKS.length - 1);
-    setCurrentIndex(index);
-  }, []);
-
   const handleWorkSelectorClick = useCallback((index: number) => {
-    setCurrentIndex(index);
-
     if (!scrollFieldRef.current) return;
-    scrollFieldRef.current.scrollTop = (index + 0.5) * workHeight;
+
+    const newScrollAmount = (index + 0.5) * workHeight;
+
+    scrollFieldRef.current.scrollTop = newScrollAmount;
+    setScrollAmount(newScrollAmount);
   }, []);
+
+  const currentIndex = Math.min(Math.trunc(scrollAmount / workHeight), WORKS.length - 1);
 
   return (
     <>
@@ -35,7 +34,11 @@ export default function Home() {
         <div className="flex w-full items-center justify-center bg-white py-12 text-lg">
           Akira Wakita Lab.
         </div>
-        <div className="scrollbar-hidden w-full grow overflow-y-scroll" onScroll={handleScrollFieldScroll} ref={scrollFieldRef}>
+        <div
+          ref={scrollFieldRef}
+          className="scrollbar-hidden w-full grow overflow-y-scroll"
+          onScroll={(event) => setScrollAmount(event.currentTarget.scrollTop)}
+        >
           <div style={{height: WORKS.length * workHeight + scrollFieldHeight}}></div>
         </div>
         <div className="flex w-full items-center justify-center bg-white px-0 py-12 md:px-8">

37ca1f8633a9c79cc54c6fba89284db734edef42
MugiSus <mmgiss256@gmail.com>
2025-03-01 20:18:26 +0900

    refactor: Update font weight for header in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
       />
 
       <main className="fixed flex h-svh w-full flex-col items-center justify-between font-light leading-loose tracking-tighter">
-        <div className="flex w-full items-center justify-center bg-white py-12 text-lg">
+        <div className="flex w-full items-center justify-center bg-white py-12 text-lg font-extrabold">
           Akira Wakita Lab.
         </div>
         <div

e2f0246c2dc8b76f7bcf7b7d14d89bdce86b3792
MugiSus <mmgiss256@gmail.com>
2025-03-13 06:27:35 +0900

    refactor: Simplify Home component by converting header to a Link element for improved navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,9 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-svh w-full flex-col items-center font-light leading-loose tracking-tighter">
-      <div className="sticky top-0 z-50 flex w-full items-center justify-center bg-white py-12 text-xl font-extrabold">
-        <Link href="#top">
-          Akira Wakita Lab.
-        </Link>
-      </div>
+      <Link className="sticky top-0 z-50 flex w-full items-center justify-center bg-white py-12 text-xl font-extrabold" href="/#top">
+        Akira Wakita Lab.
+      </Link>
 
       <div className="w-full max-w-5xl">
         {WORKS.map((work) => (

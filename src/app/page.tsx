f8c186796ee117fced1930780ac12761cfe20ab9
MugiSus <mmgiss256@gmail.com>
2025-03-13 06:31:56 +0900

    refactor: Adjust Home component layout for improved scrolling and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-svh w-full flex-col items-center font-light leading-loose tracking-tighter">
-      <Link className="sticky top-0 z-50 flex w-full items-center justify-center bg-white py-12 text-xl font-extrabold" href="/#top">
+    <main className="flex h-svh w-full flex-col items-center font-light leading-loose tracking-tighter">
+      <Link className="top-0 z-50 flex w-full items-center justify-center bg-white py-12 text-xl font-extrabold" href="#top">
         Akira Wakita Lab.
       </Link>
 
-      <div className="w-full max-w-5xl">
+      <div className="scrollbar-hidden w-full max-w-5xl flex-1 overflow-y-auto">
+        <div id="top" className="h-16" />
         {WORKS.map((work) => (
           <WorkItem key={work.id} work={work} />
         ))}

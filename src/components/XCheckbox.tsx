dbf8436d83e8da5d6f6ee5ada84686eb1b657f67
MugiSus <mmgiss256@gmail.com>
2025-03-13 00:44:44 +0900

    refactor: Enhance XCheckbox component with animated checkmark transitions

--- src/components/XCheckbox.tsx
+++ src/components/XCheckbox.tsx
@@ -3,9 +3,19 @@ export default function XCheckbox({selected}: { selected: boolean }) {
     <div className="cursor-pointer">
       <svg width="16" height="16" stroke="currentColor" fill="none" strokeWidth={1} viewBox="0 0 16 16">
         <path d="M2 2 L14 2 L14 14L2 14Z" />
-        {selected && (
-          <path d="M2 2 L14 14 M14 2L2 14Z" />
-        )}
+        <path
+          d="M2 2 L14 14"
+          className="transition-[stroke-dashoffset] duration-200 ease-in-out"
+          strokeDasharray="17"
+          // strokeDashoffset={selected ? '0' : '17'}
+          style={{strokeDashoffset: selected ? '0' : '17'}}
+        />
+        <path
+          d="M14 2 L2 14"
+          className="transition-[stroke-dashoffset] delay-100 duration-200 ease-in-out"
+          strokeDasharray="17"
+          strokeDashoffset={selected ? '0' : '17'}
+        />
       </svg>
     </div>
   );

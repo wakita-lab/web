2367fda454ad38f7d4f6a7aae59d042d5d9fc52e
MugiSus <mmgiss256@gmail.com>
2025-03-13 13:34:54 +0900

    refactor: Update Home component to use an h1 element for the title and adjust anchor links for improved navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex h-svh w-full flex-col items-center font-light leading-loose tracking-tighter">
-      <Link className="top-0 z-50 flex w-full items-center justify-center bg-white py-12 text-xl font-extrabold" href="#top">
-        Akira Wakita Lab.
+      <Link className="top-0 z-50 flex w-full items-center justify-center bg-white py-12 text-xl font-extrabold" href="#1">
+        <h1>Akira Wakita Lab.</h1>
       </Link>
 
-      <div className="scrollbar-hidden w-full max-w-5xl flex-1 overflow-y-auto">
-        <div id="top" className="h-16" />
+      <div className="scrollbar-hidden w-full max-w-5xl flex-1 overflow-y-auto scroll-smooth px-6">
+        <div id="1" />
         {WORKS.map((work) => (
           <WorkItem key={work.id} work={work} />
         ))}

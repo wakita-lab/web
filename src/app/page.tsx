9a35a1f8b67e50fa5829713eb0ca8c57e14bbc74
MugiSus <mmgiss256@gmail.com>
2025-03-16 04:16:14 +0900

    refactor: Adjust layout and positioning in Home component for improved accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex h-svh w-full flex-col items-center font-light">
-      <Link className="top-0 flex w-full items-center justify-start bg-white p-4 px-6 text-xl font-extrabold" href="#1">
+      <Link className="fixed top-0 flex w-full items-center justify-start bg-white p-4 px-6 text-xl font-extrabold" href="#1">
         <h1>Akira Wakita Lab.</h1>
       </Link>
 
-      <div className="scrollbar-hidden z-0 w-full max-w-7xl flex-1 overflow-y-auto scroll-smooth px-8 sm:px-8">
+      <div className="z-0 w-full max-w-6xl flex-1 scroll-smooth p-16 sm:px-8">
         <div id="1" />
         {WORKS.map((work) => (
           <WorkItem key={work.id} work={work} />

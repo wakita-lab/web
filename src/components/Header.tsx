69a1112b1d4b7c2d55367857081dca62348266bc
MugiSus <mmgiss256@gmail.com>
2025-03-16 06:33:18 +0900

    refactor: Change header position from fixed to sticky for improved layout behavior

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { HamburgerMenu } from './HamburgerMenu';
 
 export const Header = () => {
   return (
-    <header className="fixed inset-x-0 top-0 z-40 flex w-full justify-between">
+    <header className="sticky inset-x-0 top-0 z-40 flex w-full justify-between">
       <Link href="/" className="px-6 py-4">
         Akira Wakita Lab.
       </Link>

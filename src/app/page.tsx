2ff0c50701a2eef5181b1206a2f82368c3a77dd4
MugiSus <mmgiss256@gmail.com>
2025-03-16 09:19:33 +0900

    fix: Adjust padding in Home and StaggeredWorkList components for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { StaggeredWorkList } from '@/components/StaggeredWorkList';
 
 export default function Home() {
   return (
-    <main className="flex w-full flex-col items-center font-light">
+    <main className="flex w-full flex-col items-center px-4 pb-16 pt-4 font-light sm:px-8">
       <StaggeredWorkList />
     </main>
   );
--- src/components/StaggeredWorkList.tsx
+++ src/components/StaggeredWorkList.tsx
@@ -5,7 +5,7 @@ import seedrandom from 'seedrandom';
 
 export function StaggeredWorkList() {
   return (
-    <div className="z-0 grid w-full max-w-6xl flex-1 grid-cols-20 gap-x-2 gap-y-8 scroll-smooth px-4 pb-16 pt-4 sm:px-8">
+    <div className="z-0 grid w-full max-w-6xl flex-1 grid-cols-20 gap-x-2 gap-y-8 scroll-smooth">
       {WORKS.map((work) => {
         const rand = seedrandom(work.id);
         const colstart = Math.trunc(rand() * 5) + 1;

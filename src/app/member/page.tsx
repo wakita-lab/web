c0d5c9f204d33f534cf5bbbbbe0659eab183d5dc
MugiSus <mmgiss256@gmail.com>
2025-04-03 15:59:41 +0900

    refactor: Move member information to a new Member page for better organization

---
+++ src/app/member/page.tsx
@@ -0,0 +1,130 @@
+export default function About() {
+  return (
+    <div className="mx-auto mb-24 flex max-w-2xl flex-col justify-center gap-8 leading-loose">
+      <h2 className="font-bold">
+        MEMBER
+      </h2>
+
+      <h3 className="font-bold">教授 / Professor</h3>
+      <ul className="ml-4 list-disc">
+        <li>脇田　玲 / Akira Wakita</li>
+      </ul>
+
+      <h3 className="font-bold">大学院博士課程 / Ph.D. Student</h3>
+      <ul className="ml-4 list-disc">
+        <li>山辺 真幸 / Masaki Yamabe</li>
+        <li>傅 然 / Fu Ran</li>
+        <li>川崎 和也 / Kazuya Kawasaki</li>
+        <li>石原 航 / Ko Ishihara</li>
+      </ul>
+
+      <h3 className="font-bold">大学院修士課程 / Master Student</h3>
+      <ul className="ml-4 list-disc">
+        <li>諸藤 勇太 / Yuta Morofuji</li>
+      </ul>
+
+      <h3 className="font-bold">学部生 / Undergraduate Student</h3>
+      <ul className="ml-4 list-disc">
+        <li>小川　楽生 / Rakuki Ogawa</li>
+        <li>武谷　梨紗子 / Risako Takeya</li>
+        <li>眞鍋　創人 / Soto Manabe</li>
+        <li>佐久間 基就 / Motonari Sakuma</li>
+        <li>鈴木　一生 / Issei Suzuki</li>
+        <li>長島　康生 / Nagashima Koki</li>
+        <li>柴原　佳範 / Shibahara Yoshinori</li>
+        <li>リ　ボクカン / Li Muhan</li>
+        <li>山下　光 / Yamashita Rei</li>
+        <li>山本　愛 / Yamamoto Ai</li>
+        <li>植田　理紗子 / Ueda Risako</li>
+        <li>松村　萌々音 / Matsumura Momone</li>
+        <li>永田　一樹 / Nagata Kazuki</li>
+        <li>宇田川　まや / Udagawa Maya</li>
+        <li>徐　嘉曼 / Xu Jiaman</li>
+        <li>桑島　侑己 / Kuwashima Yuki</li>
+      </ul>
+
+      <h3 className="font-bold">卒業生 / ALUMNI</h3>
+      <ul className="ml-4 list-disc">
+        <li>丹治 基浩 / Motohiro Tanji</li>
+        <li>栗林 賢 / Satoshi Kuribayashi</li>
+        <li>堀 太誌 / Taishi Hori</li>
+        <li>関口 智子 / Tomoko Sekiguchi</li>
+        <li>渋谷 みどり / Midori Shibutani</li>
+        <li>深川 夏子　/ Natsuko Fukagawa</li>
+        <li>岩井 貴史　/ Takafumi Iwai</li>
+        <li>北田 荘平 / Sohei Kitada</li>
+        <li>渡邊 康太郎 / Kotaro Watanabe</li>
+        <li>神谷 久泰 / Hisayasu Kamiya</li>
+        <li>檜垣 万里子 / Mariko Higaki</li>
+        <li>貝嶋 一哉 / Kazuya Kaijima</li>
+        <li>石塚 賢 / Ken Ishizuka</li>
+        <li>出口 美月 / Mizuki Deguchi</li>
+        <li>服部 大祐 / Daisuke Hattori</li>
+        <li>谷口 靖太郎 / Seitaro Taniguchi</li>
+        <li>森山 篤　/ Atsushi Moriyama</li>
+        <li>川原田 えり　/ Eri Kawarada</li>
+        <li>長谷川　智祥 / Tomoyoshi Hasegawa</li>
+        <li>長島　禎 / Tadashi Nagashima</li>
+        <li>空閑 なつか / Natsuka Kuga</li>
+        <li>中嶋　亮介 / Ryosuke Nakajima</li>
+        <li>野原　恵祐 / Keisuke Nohara</li>
+        <li>平田 英子 / Hanako Hirata</li>
+        <li>斉 友華 / Yuka Sai</li>
+        <li>中村 光一 / Koichi Nakamura</li>
+        <li>本堂 莉理 / Lily Hondo</li>
+        <li>酒井 瑛一 / Eiichi Sakai</li>
+        <li>勝沼千秋 / Chiaki Katsunuma</li>
+        <li>マラン黎 / Ray Malan</li>
+        <li>田中 れな / Rena Tanaka</li>
+        <li>佐藤 巡 / Jun Sato</li>
+        <li>鈴木 貴昭 / Takaaki Suzuki</li>
+        <li>上野 道彦 / Michihiko Uenos</li>
+        <li>姉崎 祐樹 / Yuki Anezaki</li>
+        <li>小林 展啓 / Nobuhiro Kobayashi</li>
+        <li>平瀬 謙太朗 / Kentaro Hirase</li>
+        <li>藤田 貴史 / Takafumi Fujita</li>
+        <li>山口 春花 / Haruka Yamaguchi</li>
+        <li>宮脇 亮　/ Ryo Miyawaki</li>
+        <li>道幸 寛尚 / Hironao Doko</li>
+        <li>倉脇 達郎 / Tatsuro Kurawaki</li>
+        <li>金子 結 / Yui Kaneko</li>
+        <li>首藤 まり江 / Marie Shuto</li>
+        <li>戸塚 翔三 / Shozo Totsuka</li>
+        <li>根岸 明子 / Akiko Negishi</li>
+        <li>鬼久保 綾子 / Ayako Onikubo</li>
+        <li>叶多 祥子 / Shoko Kanouta</li>
+        <li>堀　晃 / Hikaru Hori</li>
+        <li>中嶋　颯 / Sou Nakajima</li>
+        <li>松根　佑眞 / Yuuma Matsune</li>
+        <li>牛込 陽介 / Yosuke Ushigome</li>
+        <li>中安　晶 / Akira Nakayasu</li>
+        <li>石井　健 / Ken Ishii</li>
+        <li>松橋百葉 / Matsuhashi Momoha</li>
+        <li>周 南一 / Zhou Nanyi</li>
+        <li>若月泰生 / Taiki Wakatsuki</li>
+        <li>矢島 光 / Hikaru Yajima</li>
+        <li>辻 航平　/ Kohei Tsuji</li>
+        <li>佐藤　鐘成 / Syosei Sato</li>
+        <li>裏出　祐太 / Yuta Urade</li>
+        <li>新見　大暉 / Taiki Niimi</li>
+        <li>松山　彩香 / Ayaka Matsuyama</li>
+        <li>寺澤　佑那 / Yuna Terasawa</li>
+        <li>瀧 隆一郎 / Ryuichirou Taki</li>
+        <li>村松　充 / Mitsuru Muramatsu</li>
+        <li>海宝　竜也 / Tatsuya Kaiho</li>
+        <li>亀田　菜都子 / Natsuko Kameda</li>
+        <li>栗原　美樹 / Miki Kurihara</li>
+        <li>大河　遼太 / Ryota Okawa</li>
+        <li>髙橋　俊成 / Seibe Takahashi</li>
+        <li>大江　智之 / Tomoyuki Oe</li>
+        <li>中野　亜希人 / Akito Nakano</li>
+        <li>田畑　諒也 / Ryoya Tabata</li>
+        <li>中村　真実 / Mami Nakamura</li>
+        <li>計良　風太 / Futa Kera</li>
+        <li>滝　ゆうり / Yuri Taki</li>
+        <li>瀧本　花乃介 / Hananosuke Takimoto</li>
+        <li>守田　幸洋 / Yukihiro Morita</li>
+      </ul>
+    </div>
+  );
+}

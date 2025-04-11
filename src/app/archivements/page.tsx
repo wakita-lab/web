6d4ab15824cbd4c7f5ccc8f2ce50cc20c5c03773
MugiSus <mmgiss256@gmail.com>
2025-04-11 20:58:59 +0900

    feat: Add awards and exhibitions section to achievements page

--- src/app/archivements/page.tsx
+++ src/app/archivements/page.tsx
@@ -5,6 +5,34 @@ export default function About() {
         ACHIEVEMENTS
       </h2>
 
+      <h3 className="font-bold">受賞</h3>
+      <ul className="ml-4 list-disc">
+        <li>グッドデザイン賞 8Kデータビジュアライゼーション [新型コロナウイルスの進化地図] 公益財団法人日本デザイン振興会（山辺真幸）, 2022.</li>
+        <li>第33期学会賞（技術賞） 新型コロナウイルスゲノム系統樹の3次元可視化 一般社団法人 可視化情報学会 （山辺真幸）, 2022.</li>
+        <li>渋谷アートアワード 2022（若月泰生）, 2022.</li>
+        <li>総務省異能vation ジェネレーションアワード 入賞（石原航）, 2021.</li>
+        <li>第49回可視化情報シンポジウム アートコンテスト 大賞（山辺真幸）, 2021.</li>
+        <li>WIRED Creative Hack Award 2021 準グランプリ（石原航）, 2021.</li>
+        <li>第27回 学生CGコンテスト 入賞（真鍋創人）, 2021.</li>
+        <li>第26回 学生CGコンテスト 入賞（石原航）, 2020.</li>
+        <li>第25回 学生CGコンテスト 審査員特別賞（石原航）, 2019.</li>
+        <li>WIRED Creative Hack Award 2018 特別賞（石原航）, 2018.</li>
+        <li>第24回 学生CGコンテスト 入賞（石原航）, 2018.</li>
+        <li>アジアデジタルアート大賞FUKUOKA 2019 入賞（野原恵祐、中嶋亮介）, 2019.</li>
+        <li>第21回 文化庁メディア芸術祭 2018, 審査委員会推薦作品（計良風太）</li>
+        <li>Asia Digital Art Award 2012, Interactive Art 部門 入賞（辻航平）, 2012.</li>
+        <li>Asia Digital Art Award 2011, Interactive Art 部門 入賞（辻航平）, 2011.</li>
+        <li>ACE 2011, Interactive Art, Gold Award 受賞, リスボン（辻航平）, 2011.</li>
+        <li>CAADRIA 2011, Best Paper Award 受賞, ニューカッスル, 2011.</li>
+        <li>インタラクション2011 インタラクティブ発表賞（&quot;Anabiosis&quot; 辻航平), 日本科学未来館, 2011.</li>
+        <li>第14回 文化庁メディア芸術祭 2010, 審査委員推薦作品, 国立新美術館, 2010.</li>
+        <li>第82回 装苑賞, 入選 (田山淳朗選出「鏡地獄」丹治基浩), 2007.</li>
+        <li>第81回 装苑賞, 入選 (滝沢直己選出, &quot;Mapping Cloth&quot; 丹治基浩), 2006.</li>
+        <li>第29回 HepFive Original Fashion Contest, 入選, (丹治基浩), 2006.</li>
+        <li>第33回 神戸ファッションコンテスト, 入選, (丹治基浩), 2006.</li>
+        <li>ACE 2006, Bronze Award (&quot;Plant Display&quot; 栗林 賢), 2006.</li>
+      </ul>
+
       <h3 className="font-bold">展示</h3>
       <ul className="ml-4 list-disc">
         <li>総務省OPEN異能Vation, 日比谷ミッドタウン（石原航）, 2023</li>
@@ -56,6 +84,13 @@ export default function About() {
         <li>TOKYO FIBER 07 SENSEWARE, 青山スパイラル (2007/4/27-29, 参加作家 山中俊治氏との共同研究として)</li>
         <li>慶應義塾大学 脇田玲研究室（堀太誌+桐山雄一+脇田玲）, アキバ犬 - Akiba Dog, 秋葉原デザインミュージアム D-Akihabara Temporary 展示, 6 Sep., 2005.</li>
         <li>慶應義塾大学 脇田玲研究室, RFID Activity Score, Good Design Presentation 2005 （Good Design Award 2次審査会）, 25-27 Aug, 2005.</li>
+        <li>未来の学校祭, 東京ミッドタウン, 2019.</li>
+        <li>アルス・エレクトロニカ・センター, Robotinity展, リンツ, 2011-2015.</li>
+        <li>文化庁メディア芸術祭 釧路展 招待出展, 北海道立釧路芸術館, 2014.</li>
+        <li>MO]VE[MENT 2009 (&quot;quatre unite&quot;, 服飾団体Fabとの共作), 東京大学総合研究博物舘, 2009.</li>
+        <li>インテリアライフスタイル 2009 (&quot;Kerido&quot;, 山一(株)との共作), 東京ビットサイト, 2009.</li>
+        <li>Tokyo Fiber 07 Senseware (&quot;Ephyra&quot;, 山中俊治氏との共作), 青山スパイラル, 2007.</li>
+        <li>インタラクティブ東京, 日本科学未来館, 2006.</li>
       </ul>
 
       <h3 className="font-bold">国際会議</h3>

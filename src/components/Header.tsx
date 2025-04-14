19d2945f1cecd6145f7e700207f2c7e66b2b0914
MugiSus <mmgiss256@gmail.com>
2025-04-14 13:24:05 +0900

    fix: Add backdrop grayscale to header for improved visibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 
   return (
     <>
-      <header className="sticky inset-x-0 top-0 z-20 flex w-full justify-between text-white mix-blend-difference">
+      <header className="sticky inset-x-0 top-0 z-20 flex w-full justify-between text-white mix-blend-difference backdrop-grayscale">
         <Link href="/" className="p-4 md:px-6">
           Akira Wakita Lab. Keio SFC
         </Link>

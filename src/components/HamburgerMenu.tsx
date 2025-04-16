f3468ee6e62392edce4b21a6498a73f80af58fa1
MugiSus <mmgiss256@gmail.com>
2025-04-17 01:57:12 +0900

    fix: Update background colors in globals.css and HamburgerMenu for improved consistency

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -9,14 +9,14 @@ interface HamburgerMenuProps {
 export const HamburgerMenu = ({ isOpen }: HamburgerMenuProps) => {
   return (
     <div
-      className={`fixed right-0 top-0 z-10 flex h-full flex-col gap-8 border-l border-gray-300 bg-white/80 px-8 pt-16 backdrop-blur-2xl transition-transform duration-300 ease-in-out ${
+      className={`fixed right-0 top-0 z-10 flex h-full flex-col gap-8 border-l border-gray-300 bg-neutral-200/80 px-8 pt-16 backdrop-blur-2xl transition-transform duration-300 ease-in-out ${
         isOpen ? 'translate-x-0' : 'translate-x-full'
       }`}
     >
       <input
         type="text"
         placeholder="Search"
-        className="w-64 border border-gray-300 px-2 py-1 text-center text-sm focus:border-gray-500 focus:outline-none"
+        className="w-64 border-b border-neutral-700 bg-transparent py-1 text-sm placeholder:text-neutral-700 focus:outline-none focus:placeholder:text-neutral-500"
       />
       <div className="flex flex-col gap-1">
         <Link href="/works">Works</Link>

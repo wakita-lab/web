2d6b1141100e9ae3d7a3c1feee40cc7974873d95
MugiSus <mmgiss256@gmail.com>
2025-04-17 04:51:37 +0900

    feat: Add framer-motion for animations in template components

---
+++ src/app/works/template.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { motion } from 'framer-motion';
+import React from 'react';
+
+const variants = {
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
+};
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      transition={{
+        type: 'linear',
+        duration: 1,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}

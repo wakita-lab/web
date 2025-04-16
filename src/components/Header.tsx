0544c0047f3a5066e96cdc3ad55ff62e9a7565c8
MugiSus <mmgiss256@gmail.com>
2025-04-17 02:51:48 +0900

    fix: Adjust header layout for improved responsiveness and consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 
   return (
     <>
-      <header className="sticky inset-x-0 top-0 z-20 flex w-full justify-between text-white mix-blend-difference backdrop-grayscale">
+      <header className="sticky inset-x-0 top-0 z-20 mx-0 flex w-auto justify-between text-white mix-blend-difference backdrop-grayscale md:mx-[100px]">
         <Link href="/" className="p-4 md:px-6">
           Akira Wakita Lab. Keio SFC
         </Link>

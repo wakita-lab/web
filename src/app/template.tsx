9771099d9c145796cf107f2157fecf1fd25c94f8
MugiSus <mmgiss256@gmail.com>
2025-04-17 04:57:17 +0900

    fix: Wrap motion component in AnimatePresence for improved animation handling

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,24 +1,29 @@
 'use client';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 
 const variants = {
   hidden: { opacity: 0 },
   enter: { opacity: 1 },
+  exit: { opacity: 0 },
 };
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <motion.div
-      variants={variants}
-      initial="hidden"
-      animate="enter"
-      transition={{
-        type: 'linear',
-        duration: 1,
-      }}
-    >
-      {children}
-    </motion.div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={Math.random()}
+        variants={variants}
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        transition={{
+          type: 'linear',
+          duration: 1,
+        }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
   );
 }

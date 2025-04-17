891705190378b10cec152dca0cbf0ba2e9aec5b8
MugiSus <mmgiss256@gmail.com>
2025-04-17 13:28:45 +0900

    fix: Remove AnimatePresence wrapper from Template component for simplification

--- src/app/works/template.tsx
+++ src/app/works/template.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import React from 'react';
 
 const variants = {
@@ -10,20 +10,17 @@ const variants = {
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={Math.random()}
-        variants={variants}
-        initial="hidden"
-        animate="enter"
-        exit="exit"
-        transition={{
-          type: 'linear',
-          duration: 1,
-        }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      transition={{
+        type: 'linear',
+        duration: 1,
+      }}
+    >
+      {children}
+    </motion.div>
   );
 }

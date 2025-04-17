0648e51ebb2c467cdb4ffc495d9a1ac27e1099fc
MugiSus <mmgiss256@gmail.com>
2025-04-17 14:18:51 +0900

    fix: Add padding to role headings for improved readability

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -30,7 +30,7 @@ export default function MemberPage() {
 
         return (
           <div key={role}>
-            <h3 className="mb-2 bg-accent">{roleHeadings[role]}</h3>
+            <h3 className="mb-2 bg-accent px-1">{roleHeadings[role]}</h3>
             <ul className="list-hyphen">
               {members.map((member) => (
                 <li key={member.name.en}>

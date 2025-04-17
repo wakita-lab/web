1687bbec9473721c36d140e36ce9ce6e6133ba9b
MugiSus <mmgiss256@gmail.com>
2025-04-17 19:12:08 +0900

    fix: Remove redundant backdrop-grayscale class from header for improved clarity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 
   return (
     <>
-      <header className="sticky inset-x-0 top-0 z-30 mx-0 flex w-auto justify-between text-white mix-blend-difference backdrop-grayscale lg:mx-[108px]">
+      <header className="sticky inset-x-0 top-0 z-30 mx-0 flex w-auto justify-between text-white mix-blend-difference lg:mx-[108px]">
         <Link href="/" className="py-3 pl-4 md:px-6">
           Akira Wakita Lab., Keio Univ. SFC
         </Link>

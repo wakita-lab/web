4ca4da4eabd191f877f88cdf18a6b42ed5c142ea
MugiSus <mmgiss256@gmail.com>
2025-04-17 10:47:28 +0900

    fix: Update z-index in Header component for improved layering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 
   return (
     <>
-      <header className="sticky inset-x-0 top-0 z-20 mx-0 flex w-auto justify-between text-white mix-blend-difference backdrop-grayscale md:mx-[108px]">
+      <header className="sticky inset-x-0 top-0 z-30 mx-0 flex w-auto justify-between text-white mix-blend-difference backdrop-grayscale md:mx-[108px]">
         <Link href="/" className="px-4 py-3 md:px-6">
           Akira Wakita Lab., Keio Univ. S.F.C.
         </Link>

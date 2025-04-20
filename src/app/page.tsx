916ba5081b78dfaa56780e2c7cbb48df32756f04
MugiSus <mmgiss256@gmail.com>
2025-04-21 02:22:34 +0900

    fix: adjust padding for work title display for better alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
                   } />
                 ))}
               </div>
-              <div className="grow overflow-hidden text-nowrap bg-neutral-50 pt-1 leading-4">
+              <div className="grow overflow-hidden text-nowrap bg-neutral-50 pt-[0.2em] leading-4">
                 {work.title.en}
               </div>
             </div>

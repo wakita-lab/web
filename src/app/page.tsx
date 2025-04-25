980f1c7aad210af7ed976d710eeeb9b3049e64ba
MugiSus <mmgiss256@gmail.com>
2025-04-25 16:26:07 +0900

    fix: Remove unused React import to clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { WORKS } from '@/constants/works';
 import { getTagColor } from '@/constants/tags';
-import React, { createRef } from 'react';
+import { createRef } from 'react';
 import CategoryLines from '@/components/CategoryLines';
 
 

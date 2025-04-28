bff8ab4d4b944c7ecc56ca343d810b98f1d3a7e0
MugiSus <mmgiss256@gmail.com>
2025-04-29 04:55:00 +0900

    fix: Adjust maximum width of MemberPage component from 2xl to lg

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -21,7 +21,7 @@ export default function MemberPage() {
   }, {} as Record<string, typeof MEMBERS>);
 
   return (
-    <div className="mx-auto mb-24 mt-8 flex max-w-2xl flex-col justify-center gap-12 px-4 leading-loose">
+    <div className="mx-auto mb-24 mt-8 flex max-w-lg flex-col justify-center gap-12 px-4 leading-loose">
       <h1 className="bg-accent text-2xl font-medium">Member</h1>
 
       {roleOrder.map(role => {

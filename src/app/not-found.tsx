eaca4ee1275d7a60cbbf0e4e9ee80acd3bd0b662
MugiSus <mmgiss256@gmail.com>
2025-05-14 20:21:36 +0900

    adjust not-found page layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="flex min-h-[calc(100vh-80px)] flex-col items-center justify-center px-8 text-center">
+    <div className="flex min-h-[calc(100svh-80px)] flex-col items-center justify-center px-8 text-center">
       <div className="work-affine-matrix-1 mb-8 mr-4 text-[120px] font-bold leading-none">
         404
       </div>
@@ -9,7 +9,7 @@ export default function NotFound() {
         Page Not Found
       </h1>
 
-      <p className="mb-12 text-neutral-700">
+      <p className="text-neutral-700">
         お探しのページは存在しないか、移動または削除された可能性があります。
       </p>
     </div>
